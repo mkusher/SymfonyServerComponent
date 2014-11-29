bd97d21213ed92b4e5f433619a21bfe3f4f009f9
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2014-11-29 14:15:34 +0300

    fixed amqp server format

--- examples/amqp-server.php
+++ examples/amqp-server.php
@@ -5,26 +5,24 @@ define("ROOT_DIR", dirname(__DIR__));
 require_once ROOT_DIR . "/vendor/autoload.php";
 
 $config = [
-
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'port' => '',
+    'vhost' => '',
+    'exchange' => '',
+    'routing_key' => ''
 ];
 
 $server = new Mkusher\SymfonyServerComponent\Amqp\RpcServer();
 
 $server->on('message', function(Mkusher\SymfonyServerComponent\Amqp\Request $request, Mkusher\SymfonyServerComponent\Server\ResponseInterface $response) use ($app){
-    try {
-        $answer = $app->handle($request);
-    }
-    catch(Exception $e){
-        $answer = new \Symfony\Component\HttpFoundation\Response();
-        $answer->setStatusCode(500);
-    }
-    $response->setAnswer($answer);
-    $response->end();
-    $app->terminate($request, $answer);
+    $response->setResponse($app->handle($request));
+    $app->terminate($request, $response->getResponse());
 });
 
 $server->run($config);
 
-echo "Login Server has started listening to queues\n";
+echo "Server has started listening to queues\n";
 
 $server->loop();
